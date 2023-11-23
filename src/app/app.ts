f295cf3a02d79acd9f2c8a3d9870c4240ebc30a0
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-11-23 14:54:20 +0600

    add readme

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,25 +3,23 @@ import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
-//parsers
+//parsers middleware
 app.use(express.json());
 app.use(cors());
 
 app.get('/health', (_req: Request, res: Response) => {
-    res.status(200).json({ success: true, message: 'Working Successfully!' });
+  res.status(200).json({ success: true, message: 'Working Successfully!' });
 });
 
 // routes
-app.use('/api/v1', () => { });
+app.use('/api', () => {});
 
 // not found
 app.all('*', (_req: Request, res: Response) => {
-    res.status(404).json({
-        success: false,
-        message: '404! Route Not found.',
-    });
+  res.status(404).json({
+    success: false,
+    message: '404! Route Not found.',
+  });
 });
-
-
 
 export default app;
--- src/app/server.ts
+++ src/app/server.ts
@@ -3,16 +3,15 @@ import app from './app';
 import config from './config';
 
 (async function () {
-    try {
-        await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string);
 
-        console.log('Database Connected!');
+    console.log('Database Connected!');
 
-        app.listen(config.port, () => {
-            console.log(`Server is listening on port:${config.port}`);
-        });
-    } catch (err) {
-        console.log(err);
-    }
-})()
-
+    app.listen(config.port, () => {
+      console.log(`Server is listening on port:${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
