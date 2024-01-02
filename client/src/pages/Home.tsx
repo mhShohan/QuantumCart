22366a0bc7b8ed19f01130bdde59ee7542c0bd82
mhshohan1 <mehdihasanshohan25@gmail.com>
2024-01-02 14:53:48 +0600

    home and productCard-1.19

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
+
+const demo = {
+  productId: '12',
+  photo: 'https://chisellabs.com/blog/wp-content/uploads/2022/05/What-is-a-Product.png',
+  name: 'string',
+  price: 24,
+  stock: 4,
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  const addToCartHandler = () => {};
+  return (
+    <div className='home'>
+      <section></section>
+
+      <h1>
+        Latest Product
+        <Link to='/search' className='findmore'>
+          More
+        </Link>
+      </h1>
+
+      <main>
+        <ProductCard {...demo} handler={addToCartHandler} />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
